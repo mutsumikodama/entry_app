4b9245c53cf91fed59775e3befe7bc031a8f2bd2
mutsumikodama <mutsumikodama+dev@gmail.com>
2015-08-09 23:52:07 +0900

    migration

--- db/migrate/20150809132911_create_comments.rb
+++ db/migrate/20150809132911_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer :id
       t.text :body
       t.string :status
       t.integer :entry_id
